9eabb926493cf7a97f2d51f7be5412048a6953e5
Vitor Risso <vrisso@lifescan.com>
2021-11-04 19:52:31 -0300

    [vitor] Using single activity

---
+++ app/src/main/java/com/example/androiproffy/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.androiproffy.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.androiproffy.R
+import com.example.androiproffy.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_splash_screen)
+    }
+}
