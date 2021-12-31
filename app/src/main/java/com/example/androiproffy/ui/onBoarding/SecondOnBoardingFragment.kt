467b466fe004ec07d104712f830dd6b3a1843c38
Vitor Risso <vrisso@lifescan.com>
2021-12-31 11:12:57 -0300

    use data binding util and implement view model on second on boarding fragment

--- app/src/main/java/com/example/androiproffy/ui/onBoarding/SecondOnBoardingFragment.kt
+++ app/src/main/java/com/example/androiproffy/ui/onBoarding/SecondOnBoardingFragment.kt
@@ -1,16 +1,20 @@
 package com.example.androiproffy.ui.onBoarding
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.example.androiproffy.R
 import com.example.androiproffy.databinding.FragmentSecondOnBoardingBinding
+import com.example.androiproffy.ui.main.MainActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SecondOnBoardingFragment : Fragment() {
 
-    private lateinit var viewModel: SecondOnBoardingFragmentViewModel
+    private val viewModel: SecondOnBoardingFragmentViewModel by viewModel()
 
     private lateinit var binding: FragmentSecondOnBoardingBinding
 
@@ -18,13 +22,19 @@ class SecondOnBoardingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentSecondOnBoardingBinding.inflate(inflater)
+        binding =
+            DataBindingUtil.inflate<FragmentSecondOnBoardingBinding>(
+                inflater,
+                R.layout.fragment_second_on_boarding,
+                container,
+                false
+            ).apply {
+                viewModel = this@SecondOnBoardingFragment.viewModel
+                lifecycleOwner = viewLifecycleOwner
+            }
+        binding.btnArrow.setOnClickListener {
+            startActivity(Intent(context, MainActivity::class.java))
+        }
         return binding.root
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SecondOnBoardingFragmentViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 }
--- app/src/main/java/com/example/androiproffy/ui/onBoarding/SecondOnBoardingFragmentViewModel.kt
+++ app/src/main/java/com/example/androiproffy/ui/onBoarding/SecondOnBoardingFragmentViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.androiproffy.ui.onBoarding
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SecondOnBoardingFragmentViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private var _shouldNavigateToNextScreen = MutableLiveData<Boolean>(false)
+    val shouldNavigateToNextScreen: LiveData<Boolean> get() = _shouldNavigateToNextScreen
+
+    fun navigateToNextScreen(): Unit {
+        _shouldNavigateToNextScreen.postValue(true)
+        Log.d("VITOR", "navigate")
+    }
 }
