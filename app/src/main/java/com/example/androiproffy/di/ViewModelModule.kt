e7a99f0ae7d4b03c73fe9108f39869d9b880de59
Vitor Risso <vrisso@lifescan.com>
2022-01-05 20:24:14 -0300

    add view models to DI

--- app/src/main/java/com/example/androiproffy/di/ViewModelModule.kt
+++ app/src/main/java/com/example/androiproffy/di/ViewModelModule.kt
@@ -1,11 +1,18 @@
 package com.example.androiproffy.di
 
+import com.example.androiproffy.ui.giveclasses.GiveClassesViewModel
+import com.example.androiproffy.ui.home.HomeViewModel
 import com.example.androiproffy.ui.onBoarding.FirstOnBoardingFragmentViewModel
 import com.example.androiproffy.ui.onBoarding.SecondOnBoardingFragmentViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { FirstOnBoardingFragmentViewModel()}
+    viewModel { FirstOnBoardingFragmentViewModel() }
+
     viewModel { SecondOnBoardingFragmentViewModel() }
+
+    viewModel { HomeViewModel() }
+
+    viewModel { GiveClassesViewModel() }
 }
