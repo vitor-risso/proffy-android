88e43c6b9700d708725a650c49f8c6ad13894601
Vitor Risso <vrisso@lifescan.com>
2022-01-05 20:24:55 -0300

    create home view model

---
+++ app/src/main/java/com/example/androiproffy/ui/home/HomeViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.androiproffy.ui.home
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    private var _shouldNavigateToGiveClasses = MutableLiveData(false)
+    val shouldNavigateToGiveClasses get() = _shouldNavigateToGiveClasses
+
+    private var _shouldNavigateToStudy = MutableLiveData(false)
+    val shouldNavigateToStudy get() = _shouldNavigateToStudy
+
+    fun navigateToGiveClasses() {
+        _shouldNavigateToGiveClasses.postValue(true)
+    }
+
+    fun navigateToStudy() {
+        _shouldNavigateToStudy.postValue(true)
+    }
+}
