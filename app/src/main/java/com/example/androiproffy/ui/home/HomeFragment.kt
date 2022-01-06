78d8489526b816bf3349d7c11b759f19359b4b29
Vitor Risso <vrisso@lifescan.com>
2022-01-06 18:38:22 -0300

    add fragment home and the navigations

--- app/src/main/java/com/example/androiproffy/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/androiproffy/ui/home/HomeFragment.kt
@@ -1,60 +1,68 @@
 package com.example.androiproffy.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.androiproffy.R
+import com.example.androiproffy.databinding.FragmentHomeBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentHomeBinding? = null
+    val binding get() = _binding!!
+    private val viewModel: HomeViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = DataBindingUtil.inflate<FragmentHomeBinding>(
+            inflater,
+            R.layout.fragment_home,
+            container,
+            false
+        ).apply {
+            viewModel = this@HomeFragment.viewModel
+            lifecycleOwner = this@HomeFragment.viewLifecycleOwner
+        }.also {
+            setObservers()
+        }
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun setObservers() {
+        viewModel.shouldNavigateToGiveClasses.observe(viewLifecycleOwner) {
+            it?.also {
+                if (it) {
+                    navigate(R.id.navigateFromHomeToGiveClasses)
                 }
             }
+        }
+
+        viewModel.shouldNavigateToStudy.observe(viewLifecycleOwner) {
+            it?.also {
+                if (it) {
+                    navigate(R.id.navigateFromHomeToStudy)
+                }
+            }
+
+        }
+    }
+
+    private fun navigate(id: Int) {
+        findNavController().navigate(id)
     }
 }
